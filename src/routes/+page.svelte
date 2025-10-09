ff97706d33d5d55ecaa53d4de9b06510a6ec7e0c
Ivan Nance <ivanluvswaffles@gmail.com>
2025-10-08 22:50:19 -0400

    added title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>Ivan's World</title>
+</svelte:head>
+
 <div class="flex justify-center items-center w-screen h-screen bg-blue-500">
-	<h1 class="text-6xl font-bold">Hello World!</h1>
+	<h1 class="text-6xl font-bold text-white">Hello World!</h1>
 </div>
